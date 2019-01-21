3e2f9ff77e98b4355866ca3b949948d550c406f1
Ionut Djon Iancu <43295244+IonutDjonIancu@users.noreply.github.com>
2019-01-21 05:50:51 -0800

    21.01.2019

---
+++ numeric_data_types.py
@@ -0,0 +1,17 @@
+'''https://docs.google.com/spreadsheets/d/1vT9TCy69l9CAXTH3W-DkJn1HrTQv9MzB8iBY1zDp6zI/edit#gid=0'''
+
+
+
+price_of_rice = 10
+print(price_of_rice)
+
+price_of_rice = 20.0
+print(price_of_rice)
+
+rice = "Rice"
+print(rice)
+
+print(price_of_rice * 2)
+
+
+
