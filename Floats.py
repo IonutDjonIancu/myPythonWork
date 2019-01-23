df47cc603bf468bdfdffbd44d26b03285ca665b2
ionutdjoniancu <iiancu85@gmail.com>
2019-01-23 15:44:37 -0800

    23.01.2019

---
+++ Floats.py
@@ -0,0 +1,5 @@
+print(1.4)
+print(4.333333333)
+print('%.2f' % 4.333333333)  # this prints a specific number of decimals
+
+
---
+++ Integers.py
@@ -0,0 +1,62 @@
+# This is a single line comment
+# This is second line comment
+# Ctrl-D duplicates the line
+# This is second line comment
+# Ctrl-Shift-arrowkey moves the line up and down
+
+"""
+this is a multi line
+comment
+"""
+
+
+"""Arithmetic Operations"""
+# Addition
+# the addition operator: +
+print(2+3)
+
+# Subtraction
+# the subtraction operator: -
+print(2-3)
+
+# Multiplication
+# the multiplication operator: *
+print(2*3)
+
+# Division
+# the division operator: /
+print(2/3)
+print(7//3)
+
+# Modulus Division
+# the division operator: %
+print(3 % 2)
+print(7 % 3)
+print(2 % 3)
+
+# Exponentiation
+# the expo operator: **
+print(5 ** 2)
+
+
+"""Order of operations"""
+print(1+2*3**4//5)  # uses a double forward slash division operator, to obtain a integer division
+print(1+2*3**4/5)
+print(int(1+2*3**4/5))
+
+"""Casting"""
+# int()
+# float()
+
+print(float(1))
+print(int(1.1))
+
+
+
+
+
+
+
+
+
+
