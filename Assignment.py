ec9c64de2b56a2dd0ca803f34c47068adbedb7e2
ionutdjoniancu <iiancu85@gmail.com>
2019-01-23 15:46:04 -0800

    21.01.2019

--- Assignment.py
+++ Assignment.py
@@ -22,6 +22,16 @@ the_price_of_cocaine /= 5
 print(the_price_of_cocaine)
 print(int(the_price_of_cocaine))
 
+# Assignment operator: //=
+the_price_of_cocaine //= 5
+print(the_price_of_cocaine)
+print(int(the_price_of_cocaine))
+
+# Assignment operator: **=
+the_price_of_cocaine **= 2
+print(the_price_of_cocaine)
+print(int(the_price_of_cocaine))
+
 # Applying vat
 the_price_of_cocaine += the_price_of_cocaine * vat
 print(the_price_of_cocaine)
